d4308c610a10630e1b8664742a22ca68c69be752
SeokGeunHo <45616952+seok6555@users.noreply.github.com>
2023-09-06 22:45:57 +0900

    2023.09.06. 파이썬 연산, 입출력 실습
    파이썬의 변수의 자료형에 따른 연산의 결과 확인. 사용자의 입력을 받고 출력하는 실습 진행.

---
+++ 20230906Python/operation.py
@@ -0,0 +1,29 @@
+a = 1
+b = 1
+print(a, b)
+
+a = 1.5
+b = 3.5
+print(a, b)
+
+a = "ABC"
+b = "101010"
+print(a, b)
+
+a = True
+b = False
+print(a, b)
+
+print(7 // 2)
+print(7.2 // 2.2)
+print(7 % 2)
+
+a = '76.3'
+b = float(a)
+print(a)
+print(b)
+#print(a + b) -> error
+
+print("Enter your name: ")
+somebody = input()
+print("Hi", somebody, "How are you today?")
